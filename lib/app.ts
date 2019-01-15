bc7fb771c8750c488655b9206650c35adfc6144e
David Dinh <dmdinh22@gmail.com>
2019-01-15 00:17:13 -0800

    basic GET route

--- lib/app.ts
+++ lib/app.ts
@@ -1,22 +1,23 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { Routes } from './routes/api/basicRoutes';
 
 class App {
-
     public app: express.Application;
+    public routePath: Routes = new Routes();
 
     constructor() {
         this.app = express();
         this.config();
+        this.routePath.routes(this.app);
     }
 
-    private config(): void{
+    private config(): void {
         // support application/json type post data
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
-
 }
 
 export default new App().app;

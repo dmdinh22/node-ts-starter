6db2304215fdfae10bf7b831fbb17a4f7d3fa80d
David Dinh <dmdinh22@gmail.com>
2019-01-21 00:36:33 -0800

    hook up mongodb

--- lib/app.ts
+++ lib/app.ts
@@ -1,15 +1,26 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Routes } from './routes/api/basicRoutes';
+import * as mongoose from 'mongoose';
 
 class App {
     public app: express.Application;
     public routePath: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://localhost/ContactDb';
 
     constructor() {
         this.app = express();
         this.config();
         this.routePath.routes(this.app);
+        this.mongoSetup();
+    }
+
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(
+            this.mongoUrl,
+            { useNewUrlParser: true }
+        );
     }
 
     private config(): void {

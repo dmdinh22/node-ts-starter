cdb236934109d3b4eec42e2c01a934880f0655f9
David Dinh <dmdinh22@gmail.com>
2019-01-28 00:55:53 -0800

    added CRUD routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,9 +15,47 @@ main.use(bodyParser.urlencoded({ extended: false }));
 // a parameter
 export const webApi = functions.https.onRequest(main);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });import * as functions from 'firebase-functions';
+// Add new contact
+app.post('/contacts', (req, res) => {
+    firebaseHelper.firestore.createNewDocument(
+        db,
+        contactsCollection,
+        req.body
+    );
+    res.send('Create a new contact');
+});
+
+// Update new contact
+app.patch('/contacts/:contactId', (req, res) => {
+    firebaseHelper.firestore.updateDocument(
+        db,
+        contactsCollection,
+        req.params.contactId,
+        req.body
+    );
+    res.send('Update a new contact');
+});
+
+// View a contact
+app.get('/contacts/:contactId', (req, res) => {
+    firebaseHelper.firestore
+        .getDocument(db, contactsCollection, req.params.contactId)
+        .then(doc => res.status(200).send(doc));
+});
+
+// View all contacts
+app.get('/contacts', (req, res) => {
+    firebaseHelper.firestore
+        .backup(db, contactsCollection)
+        .then(data => res.status(200).send(data));
+});
+
+// Delete a contact
+app.delete('/contacts/:contactId', (req, res) => {
+    firebaseHelper.firestore.deleteDocument(
+        db,
+        contactsCollection,
+        req.params.contactId
+    );
+    res.send('Document deleted');
+});
